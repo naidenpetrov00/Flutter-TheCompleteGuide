87a235c81ce7b8b35c12816b904e89561f045d46
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-08-29 11:41:20 +0300

    shopiing_list_app

---
+++ shopping_list_app/lib/screens/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: const Text('Your Grocceries'),
+      ),
+    );
+  }
+}
