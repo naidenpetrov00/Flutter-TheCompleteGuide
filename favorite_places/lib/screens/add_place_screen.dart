b073bffc95251b38e2dc27f4b3de7612ac05a053
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-09-25 17:29:30 +0300

    Add Init UI

--- favorite_places/lib/screens/add_place_screen.dart
+++ favorite_places/lib/screens/add_place_screen.dart
@@ -1,15 +1,18 @@
+import 'package:favorite_places/providers/places_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddPlaceScreen extends StatefulWidget {
+class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
 
   @override
-  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
 }
 
-class _AddPlaceScreenState extends State<AddPlaceScreen> {
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _formKey = GlobalKey<FormState>();
 
+  late List<String> places;
   var _title = '';
   var _isSendingTheData = false;
 
@@ -19,38 +22,44 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
       setState(() {
         _isSendingTheData = true;
       });
+
+      Navigator.of(context).pop(_title);
     }
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Add new place'),
-        ),
-        body: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    label: Text('Title'),
-                  ),
-                  validator: (value) {
-                    if (value == null ||
-                        value.isEmpty ||
-                        value.trim().length < 2 ||
-                        value.trim().length > 50) {
-                      return 'Must be between 2 and 50';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _title = value!,
+  Widget build(BuildContext context) {
+    places = ref.watch(placesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new place'),
+      ),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Title'),
                 ),
-                ElevatedButton(
-                  onPressed: _isSendingTheData ? null : _saveItem,
-                  child: const Text('Add Item'),
-                ),
-              ],
-            )),
-      );
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length < 2 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 2 and 50';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _title = value!,
+              ),
+              ElevatedButton(
+                onPressed: _isSendingTheData ? null : _saveItem,
+                child: const Text('Add Item'),
+              ),
+            ],
+          )),
+    );
+  }
 }
--- favorite_places/lib/screens/my_places_screen.dart
+++ favorite_places/lib/screens/my_places_screen.dart
@@ -1,25 +1,53 @@
+import 'package:favorite_places/providers/places_provider.dart';
 import 'package:favorite_places/screens/add_place_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyPlacesScreen extends StatelessWidget {
+class MyPlacesScreen extends ConsumerStatefulWidget {
   const MyPlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Your Places'),
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (addPlaceScreenContext) => const AddPlaceScreen(),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.add),
-            ),
-          ],
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyPlacesScreenState();
+}
+
+class _MyPlacesScreenState extends ConsumerState {
+  @override
+  Widget build(BuildContext context) {
+    final places = ref.watch(placesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final place = await Navigator.of(context).push<String>(
+                MaterialPageRoute(
+                  builder: (addPlaceScreenContext) => const AddPlaceScreen(),
+                ),
+              );
+
+              setState(() {
+                places.add(place!);
+              });
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: places.length,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.only(left: 16, top: 16),
+          child: Text(
+            places[index],
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
         ),
-      );
+      ),
+    );
+  }
 }
