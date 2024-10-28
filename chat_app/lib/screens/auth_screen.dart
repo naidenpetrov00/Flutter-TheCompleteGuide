75e6370ccffa0694c545b54d8649ac30eb7890cb
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-10-28 18:15:38 +0200

    add firebase sdk

--- chat_app/lib/screens/auth_screen.dart
+++ chat_app/lib/screens/auth_screen.dart
@@ -8,6 +8,21 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreen extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLoginMode = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+
+    if (isValid) {
+      _form.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -18,7 +33,11 @@ class _AuthScreen extends State<AuthScreen> {
               children: [
                 Container(
                   margin: const EdgeInsets.only(
-                      top: 32, bottom: 24, left: 24, right: 24),
+                    top: 32,
+                    bottom: 24,
+                    left: 24,
+                    right: 24,
+                  ),
                   width: 200,
                   child: Image.asset('assets/images/chat.png'),
                 ),
@@ -28,14 +47,59 @@ class _AuthScreen extends State<AuthScreen> {
                     child: Padding(
                       padding: const EdgeInsets.all(16),
                       child: Form(
-                          child: Column(
-                        children: [
-                          TextFormField(
-                            decoration: const InputDecoration(
-                                labelText: 'Email address'),
-                          ),
-                        ],
-                      )),
+                        key: _form,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Email address'),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter valid email address';
+                                }
+
+                                return null;
+                              },
+                              onSaved: (value) => _enteredEmail = value!,
+                            ),
+                            TextFormField(
+                              decoration:
+                                  const InputDecoration(labelText: 'Password'),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value == null || value.trim().length < 6) {
+                                  return 'Password must be at least 6 characters long';
+                                }
+
+                                return null;
+                              },
+                              onSaved: (value) => _enteredPassword = value!,
+                            ),
+                            const SizedBox(height: 16),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer),
+                              onPressed: _submit,
+                              child: Text(_isLoginMode ? 'Login' : 'Sign up'),
+                            ),
+                            TextButton(
+                              onPressed: () => setState(() {
+                                _isLoginMode = !_isLoginMode;
+                              }),
+                              child: Text(_isLoginMode
+                                  ? 'Create an account'
+                                  : 'I alredy have an account'),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
