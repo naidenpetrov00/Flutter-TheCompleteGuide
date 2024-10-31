f3378553ecdf1f6bc8d0c27b7840054c03363039
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-10-31 17:07:39 +0200

    Finial Commit

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,39 +1,67 @@
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
-  Widget build(BuildContext context) => StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chat')
-            .orderBy('create_at', descending: false)
-            .snapshots(),
-        builder: (ctx, chatSnapshot) {
-          if (chatSnapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
-            return const Center(
-              child: Text('No messages'),
-            );
-          }
-          if (chatSnapshot.hasError) {
-            return const Center(
-              child: Text('Something went wrong'),
-            );
-          }
+  Widget build(BuildContext context) {
+    final userAuthenticated = FirebaseAuth.instance.currentUser!;
 
-          final loadedMessages = chatSnapshot.data!.docs;
-          return ListView.builder(
-            itemCount: loadedMessages.length,
-            itemBuilder: (ctx, index) => Text(
-              loadedMessages[index].data()['text'],
-            ),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('create_at', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        },
-      );
+        }
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages'),
+          );
+        }
+        if (chatSnapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+
+        final loadedMessages = chatSnapshot.data!.docs;
+        return ListView.builder(
+            padding: const EdgeInsets.only(bottom: 40, left: 16, right: 16),
+            reverse: true,
+            itemCount: loadedMessages.length,
+            itemBuilder: (ctx, index) {
+              final chatMessage = loadedMessages[index].data();
+              final nextChatMessage = index + 1 < loadedMessages.length
+                  ? loadedMessages[index + 1]
+                  : null;
+
+              final currentMessageUserId = chatMessage["user_id"];
+              final nextMessageUserId =
+                  nextChatMessage != null ? nextChatMessage["user_id"] : null;
+              final nextUserIsSame = nextMessageUserId == currentMessageUserId;
+              if (nextUserIsSame) {
+                return MessageBubble.next(
+                  message: chatMessage["text"],
+                  isMe: userAuthenticated.uid == currentMessageUserId,
+                );
+              } else {
+                return MessageBubble.first(
+                  message: chatMessage["text"],
+                  isMe: userAuthenticated.uid == currentMessageUserId,
+                  userImage: chatMessage['user_image'],
+                  username: chatMessage['username'],
+                );
+              }
+            });
+      },
+    );
+  }
 }
